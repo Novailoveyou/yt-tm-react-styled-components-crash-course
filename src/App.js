cc13d5c970cb74c311185525ea5df1642bef6993
novailoveyou <novailoveyoux@gmail.com>
2021-09-21 23:53:39 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { ThemeProvider } from 'styled-components'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import Card from './components/Card'
 import { Container } from './components/styled/Container.styled'
 import GlobalStyles from './components/styled/Global'
+import content from './content'
 
 const theme = {
   colors: {
     header: '#ebfbff',
     body: '#fff',
     footer: '#003333'
-  }
+  },
+  mobile: '768px'
 }
 
 function App() {
@@ -18,8 +22,11 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World</h1>
+          {content.map((item, idx) => (
+            <Card key={item.title} item={item} />
+          ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   )
